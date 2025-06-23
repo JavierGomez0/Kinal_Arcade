0d08603cc12216d6fb68f918a0404bff6131e457
Mcho2021304 <mcho-2021304@kinal.edu.gt>
2025-06-23 11:16:33 -0600

    Agregue cambios a la clase Main

--- KA/src/org/kinalarcade/system/main.java
+++ KA/src/org/kinalarcade/system/main.java
@@ -1,20 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.kinalarcade.system;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author jgome
  */
-public class main {
+public class main extends Application{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/org/kinalarcade/view/WordleView.fxml"));
+        Scene scene = new Scene(root, 550, 650); 
+        stage.setTitle("Wordle en JavaFX");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
     
+    public static void main(String[] args) {
+        launch(args);
+    } 
 }
