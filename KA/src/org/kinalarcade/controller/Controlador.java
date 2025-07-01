a5109cc2b87494e0aa1a29695a66b9ecceb20b85
Informatica <informatica@alumno.local>
2025-07-01 11:53:57 -0600

    Se Modifico el Controlador

--- KA/src/org/kinalarcade/controller/Controlador.java
+++ KA/src/org/kinalarcade/controller/Controlador.java
@@ -4,10 +4,91 @@
  */
 package org.kinalarcade.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.util.Random;
+
+import javafx.animation.PauseTransition;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+import org.kinalarcade.system.Main;
+
 /**
  *
  * @author informatica
  */
-public class Controlador {
-    
+public class Controlador implements Initializable {
+
+    private Main principal;
+
+    @FXML
+    private Label resultado;
+
+    private PauseTransition pausa;
+
+    public void setPrincipal(Main principal) {
+        this.principal = principal;
+    }
+
+    public static String jugar(String jugador) {
+        String[] opciones = {"Piedra", "Papel", "Tijera"};
+        Random random = new Random();
+
+        String maquina = opciones[random.nextInt(3)];
+
+        if (jugador.equals(maquina)) {
+            return "Empate ambos elijieron: " + jugador;
+        } else if ((jugador.equals("Piedra") && maquina.equals("Tijera"))
+                || (jugador.equals("Papel") && maquina.equals("Piedra"))
+                || (jugador.equals("Tijera") && maquina.equals("Papel"))) {
+            return "Ganaste la maquina eligio " + maquina;
+        } else {
+            return "Perdiste maquina eligio " + maquina;
+        }
+    }
+
+    @FXML
+    private void jugarPiedra() {
+        manejarClic();
+
+        resultado.setText(jugar("Piedra"));
+
+    }
+
+    @FXML
+    private void jugarPapel() {
+        manejarClic();
+
+        resultado.setText(jugar("Papel"));
+
+    }
+
+    @FXML
+    private void jugarTijera() {
+        manejarClic();
+
+        resultado.setText(jugar("Piedra"));
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Controlador cargado correctamente");
+        pausa = new PauseTransition(Duration.seconds(5));
+        pausa.setOnFinished(event -> resultado.setText("Elija su jugada..."));
+
+    }
+
+    @FXML
+    private void manejarClic() {
+        // Detenemos cualquier pausa anterior (si es que se hizo doble clic)
+        pausa.stop();
+
+        // Iniciamos una nueva espera de 10 segundos
+        pausa.play();
+    }
+
 }
--- KA/src/org/kinalarcade/system/main.java
+++ KA/src/org/kinalarcade/system/main.java
@@ -8,14 +8,58 @@ package org.kinalarcade.system;
  *
  * @author jgome
  */
-public class main {
 
-    /**
-     * @param args the command line arguments
-     */
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.kinalarcade.controller.Controlador;
+
+/**
+ *
+ * @author informatica
+ */
+public class Main extends Application {
+
+    private Stage escenarioPrincipal;
+
     public static void main(String[] args) {
-        // TODO code application logic 
-        System.out.println("Holis mundo");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage escenario) throws Exception {
+        this.escenarioPrincipal = escenario;
+        unoVs1();
+        escenario.show();
+        escenario.getIcons().add(new Image("/org/migueltamat/image/logo_piedra_papel_y_tijera-removebg-preview.png"));
+
+    }
+
+    public FXMLLoader cambiarEscena(String fxml, double Ancho, double Alto) {
+        FXMLLoader cargadorFXML = null;
+        try {
+            cargadorFXML = new FXMLLoader(getClass().getResource("/org/kinalarcade/view/" + fxml));
+            Parent ArchivoFXML = cargadorFXML.load();
+            Scene escena = new Scene(ArchivoFXML, Ancho, Alto);
+            escenarioPrincipal.setScene(escena);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return cargadorFXML;
+    }
+
+    public void unoVs1() {
+        Controlador un = cambiarEscena("PiedraPapelYTijera.view.fxml", 700, 400).getController();
+        un.setPrincipal(this);
+    }
+
+   
     
 }
+
