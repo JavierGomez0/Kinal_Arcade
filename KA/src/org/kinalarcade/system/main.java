68bf683150deff48344b820b615d6806088b9c33
Daniel010 <danieledusc@gmail.com>
2025-07-02 10:52:03 -0600

    Se cargo el proyecto

--- KA/src/org/kinalarcade/system/main.java
+++ KA/src/org/kinalarcade/system/main.java
@@ -1,20 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.kinalarcade.system;
 
-/**
- *
- * @author jgome
- */
-public class main {
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.kinalarcade.controller.MenuController;
+import org.kinalarcade.controller.GameController;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Main extends Application {
+
+    private static final String URL_VIEW = "/org/kinalarcade/View/";
+    private Stage escenarioPrincipal;
+
     public static void main(String[] args) {
-        // TODO code application logic 
+        launch(args); // <- Estes método lanza la aplicación JavaFX
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.escenarioPrincipal = stage;
+        Menu(); // Cambia a la vista GameView.fxml
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+    }
+
+    public FXMLLoader cambiarEscena(String fxml, double ancho, double alto) {
+        FXMLLoader cargadorFXML = null;
+        try {
+            cargadorFXML = new FXMLLoader(getClass().getResource(URL_VIEW + fxml));
+            Parent archivoFXML = cargadorFXML.load();
+            Scene escena = new Scene(archivoFXML, ancho, alto);
+            escenarioPrincipal.setScene(escena);
+        } catch (IOException ex) {
+            System.out.println("Error al cambiar: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return cargadorFXML;
     }
     
+    
+    public void Menu() {
+        FXMLLoader loader = cambiarEscena("MenuView.fxml", 400, 300);
+        if (loader != null) {
+            MenuController mpc = loader.getController();
+            mpc.setPrincipal(this);
+        }
+    }
+    public void Game() {
+        FXMLLoader loader = cambiarEscena("GameView.fxml", 369, 370);
+        if (loader != null) {
+            GameController mpc = loader.getController();
+            mpc.setPrincipal(this);
+        }
+    }
+
+    public void startGame() {
+       Game();
+    }
 }
