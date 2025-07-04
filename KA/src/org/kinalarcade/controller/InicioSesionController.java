dcb584e87d8f9cd1cdca64d60a7b492021439509
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2025-07-03 22:56:01 -0600

    Se modifico el controller para que ajuste la imagen automaticamente

--- KA/src/org/kinalarcade/controller/InicioSesionController.java
+++ KA/src/org/kinalarcade/controller/InicioSesionController.java
@@ -1,14 +1,14 @@
 package org.kinalarcade.controller;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import org.kinalarcade.system.main;
-
 
 /**
  * FXML Controller class
@@ -17,28 +17,35 @@ import org.kinalarcade.system.main;
  */
 public class InicioSesionController implements Initializable {
 
+    @FXML
+    private ImageView imageViewFondo;
+
+    @FXML
+    private AnchorPane anchorPane;
     private main principal;
-    
-    @FXML private Button btnMenu, btnRegistro;
+
+    @FXML
+    private Button btnMenu, btnRegistro;
 
     public void setPrincipal(main principal) {
         this.principal = principal;
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        imageViewFondo.fitWidthProperty().bind(anchorPane.widthProperty());
+        imageViewFondo.fitHeightProperty().bind(anchorPane.heightProperty());
+
+    }
+
     @FXML
-    public void clicManejoEvento(ActionEvent evento){
+    public void clicManejoEvento(ActionEvent evento) {
         //btnMenu
-        if (evento.getSource()==btnMenu){
+        if (evento.getSource() == btnMenu) {
             System.out.println("Nos vamos al Menu Principal");
             principal.menuPrincipal();
-        } 
+        }
     }
-    
-}
-    
 
+}
